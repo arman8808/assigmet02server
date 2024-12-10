ec0e252487d2d8ef48159c69d4692c42a198d8b7
arman8808 <armanal3066@gmail.com>
2024-12-10 15:43:18 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,52 +1,52 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-
-// const connectDB = async () => {
-//   console.log("Checking database URL:", process.env.dateBaseUrl);
-
-//   try {
-//     await mongoose.connect(`${process.env.dateBaseUrl}filterdataset`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB connected...");
-//   } catch (error) {
-//     console.error("Database connection error:", error.message);
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
-require("dotenv").config();
-// Ensure that the environment variable for MongoDB URI is set correctly
-console.log("DB: ->", process.env.dateBaseUrl);
-
-mongoose.set("strictQuery", true);
+require('dotenv').config();
 
 const connectDB = async () => {
+  console.log("Checking database URL:", process.env.dateBaseUrl);
+
   try {
     await mongoose.connect(`${process.env.dateBaseUrl}filterdataset`, {
-      serverSelectionTimeoutMS: 5000, // Shorten the time to try connecting
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("mongoose is connected");
-
-    mongoose.connection.on("error", (err) => {
-      console.log("MongoDB connection error:", err);
-    });
-
-    mongoose.connection.on("connected", () => {
-      console.log(" connected successfully");
-    });
-  } catch (err) {
-    console.log("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit if connection fails
+    console.log("MongoDB connected...");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    console.error(error);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// // Ensure that the environment variable for MongoDB URI is set correctly
+// console.log("DB: ->", process.env.dateBaseUrl);
+
+// mongoose.set("strictQuery", true);
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`${process.env.dateBaseUrl}filterdataset`, {
+//       serverSelectionTimeoutMS: 5000, // Shorten the time to try connecting
+//       socketTimeoutMS: 45000, // Close sockets after 45 seconds
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("mongoose is connected");
+
+//     mongoose.connection.on("error", (err) => {
+//       console.log("MongoDB connection error:", err);
+//     });
+
+//     mongoose.connection.on("connected", () => {
+//       console.log(" connected successfully");
+//     });
+//   } catch (err) {
+//     console.log("Error connecting to MongoDB:", err);
+//     process.exit(1); // Exit if connection fails
+//   }
+// };
+
+// module.exports = connectDB;
