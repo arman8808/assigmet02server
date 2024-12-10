0c4b9acfce1647d48c6ebd572415479b4ccb001a
arman8808 <armanal3066@gmail.com>
2024-12-10 15:03:48 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-console.log(process.env.PORT,'loging from server.js');
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
